97cf72b8f67177a904bf4478a4a5b15c9d6b4b90
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2023-11-14 22:35:39 +0000

    Deploying to gh-pages from @ slipchenkoanastasiia/goit-js-hw-09@163993be8b0beb79f3cad0a3c559abe44f041723 🚀

---
+++ 01-color-switcher.1a3eaae6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=e}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));let e=null;
+//# sourceMappingURL=01-color-switcher.1a3eaae6.js.map
